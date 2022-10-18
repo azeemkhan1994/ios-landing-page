e2cc29b470a747d3cecb45f9dfb4872b0c5889e3
azeemkhan1994 <azeem.khan@pebtechsolutions.com>
2022-10-18 16:30:44 +0500

    add all script tag

--- js/script.js
+++ js/script.js
@@ -128,6 +128,15 @@
       ]
     });
 
+    $('.single-item').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 1500,
+    });
 
     // scroll
     // $('.scrollTo').on('click', function (e) {
